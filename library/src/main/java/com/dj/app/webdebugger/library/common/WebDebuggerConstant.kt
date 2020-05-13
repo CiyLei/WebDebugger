5fd621e9304f69517eb84189645c94bff049615d
cl <903736662@qq.com>
2020-05-13 17:11:21 +0800

    opt: RequestCode优化

--- library/src/main/java/com/dj/app/webdebugger/library/common/WebDebuggerConstant.kt
+++ library/src/main/java/com/dj/app/webdebugger/library/common/WebDebuggerConstant.kt
@@ -7,15 +7,15 @@ package com.dj.app.webdebugger.library.common
 
 internal object WebDebuggerConstant {
     // 开启资源服务器的请求码
-    val PERMISSION_START_RESOURECE = 1001
+    val PERMISSION_START_RESOURECE = 7801001
     val RESOURCE_SERVER_FAILED_TO_OPEN = "资源服务器开启失败，请在设置中开启写入文件的权限，再重启App"
     // 发起截屏的request
-    val REQUEST_SCREEN_CAPTURE = 1002
+    val REQUEST_SCREEN_CAPTURE = 7801002
     val SCREEN_CAPTURE_FAILED = "截屏权限申请失败"
     // 发起录屏的request
-    val REQUEST_SCREEN_RECORDING = 1003
+    val REQUEST_SCREEN_RECORDING = 7801003
     val SCREEN_RECORDING_FAILED = "录屏权限申请失败"
     // 获取手机设备码的请求码
-    val PERMISSION_PHONE_STATE_RESOURECE = 1004
+    val PERMISSION_PHONE_STATE_RESOURECE = 7801004
     val RESOURCE_PHONE_STATE_FAILED_TO_OPEN = "获取手机设备码失败，无法开启连接服务"
 }
